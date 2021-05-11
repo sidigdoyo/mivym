47ade5ccddd4bb7ccfaff7291d4702b22bbd4daa
Sidigdoyo Pribadi <sidigdoyo@wagelys-MacBook-Pro.local>
2021-05-11 22:31:09 +0700

    fix(themes): remove clock in terminal themes

--- bash_it/themes/sidigdoyo/sidigdoyo.theme.bash
+++ bash_it/themes/sidigdoyo/sidigdoyo.theme.bash
@@ -13,22 +13,9 @@ GIT_THEME_PROMPT_SUFFIX="${green}|"
 RVM_THEME_PROMPT_PREFIX="|"
 RVM_THEME_PROMPT_SUFFIX="|"
 
-__bobby_clock() {
-  printf "$(clock_prompt) "
-
-  if [ "${THEME_SHOW_CLOCK_CHAR}" == "true" ]; then
-    printf "$(clock_char) "
-  fi
-}
-
 function prompt_command() {
     #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
     PS1="\n${reset_color}in ${purple}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
 }
-
-THEME_SHOW_CLOCK_CHAR=${THEME_SHOW_CLOCK_CHAR:-"true"}
-THEME_CLOCK_CHAR_COLOR=${THEME_CLOCK_CHAR_COLOR:-"$red"}
-THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$bold_cyan"}
-THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%Y-%m-%d %H:%M:%S"}
 
 safe_append_prompt_command prompt_command
